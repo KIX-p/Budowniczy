e5c8e6d1e90a458a92029651d8680ba7a1eb6987
Paweł Kikh <82141828+KIX-p@users.noreply.github.com>
2023-05-13 11:11:52 +0200

    Update Director.java

--- src/director/Director.java
+++ src/director/Director.java
@@ -1,2 +1,29 @@
-package director;public class Director {
+package director;
+
+import builder.Builder;
+import product.engine.Engine;
+
+public class Director {
+
+    public void makeMazdaRX7(Builder builder){
+        builder.reset();
+        builder.setColor("black");
+        builder.setDoors(2);
+        Engine engine = new Engine();
+        engine.setCapacity(1300);
+        engine.setPower(400);
+        engine.setType("wankl");
+        engine.setTypeOfFuel("petrol");
+    }
+    public void makeSkylineR34(Builder builder){
+        builder.reset();
+        builder.setColor("red");
+        builder.setDoors(2);
+        Engine engine = new Engine();
+        engine.setCapacity(1500);
+        engine.setPower(276);
+        engine.setType("twin-turbo");
+        engine.setTypeOfFuel("petrol");
+    }
 }
+
