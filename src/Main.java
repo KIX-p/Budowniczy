00cddb510c4843aac9943b645f488037bfd54dab
Paweł Kikh <82141828+KIX-p@users.noreply.github.com>
2023-05-13 11:10:52 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*
+        TODO:
+        * dodać jeszcze jeden samochód
+        * w main stworzyć dwa samochody oraz dwie instrukcje obsługi
+        * wysłać na teamsach link do gita
+
+         */
+
+        Director director = new Director();
+        CarBuilder builder = new CarBuilder();
+
+        director.makeMazdaRX7(builder);
+        Car MazdaRX7 = builder.getResult();
+        System.out.println(MazdaRX7);
+
+        ManualBuilder builderm = new ManualBuilder();
+        director.makeMazdaRX7(builder);
+        System.out.println(builderm);
+
+
+
+        director.makeSkylineR34(builder);
+        Car skyline = builder.getResult();
+        System.out.println(skyline);
+
+        ManualBuilder builderz = new ManualBuilder();
+        director.makeMazdaRX7(builderz);
     }
 }
